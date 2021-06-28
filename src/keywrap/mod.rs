40d03bb9db2381b84de44a77808e65b972561812
Chris Porter <porter@ibm.com>
2021-06-28 12:53:23 -0500

    first cut of keywrapper trait
    Signed-off-by: Chris Porter <porter@ibm.com>

---
+++ src/keywrap/mod.rs
@@ -0,0 +1,36 @@
+pub mod pkcs11;
+
+use crate::config::CryptoConfig;
+use crate::config::EncryptConfig;
+use crate::config::DecryptConfig;
+use std::collections::HashMap;
+
+trait KeyWrapper {
+    fn wrap_keys(&self,
+                 ec: &EncryptConfig,
+                 opts_data: &Vec<u8>)
+                 -> Result<Vec<u8>, std::io::Error>;
+
+    fn unwrap_key(&self,
+                  cfg: &DecryptConfig,
+                  annotation: &Vec<u8>)
+                  -> Result<Vec<u8>, std::io::Error>;
+
+    fn get_annotation_id(&self) -> &str;
+
+    fn no_possible_keys(&self,
+                        dcparameters: &HashMap<String, Vec<Vec<u8>>>)
+                        -> bool;
+
+    fn get_private_keys<'a>(&'a self,
+                            dcparameters: &'a HashMap<String, Vec<Vec<u8>>>)
+                            -> &'a Vec<Vec<u8>>;
+
+    fn get_key_ids_from_packet(&self,
+                               packet: String)
+                               -> Result<Vec<u64>, std::io::Error>;
+
+    fn get_recipients(&self,
+                      packet: String)
+                      -> Result<Vec<String>, std::io::Error>;
+}
