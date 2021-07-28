c13ebf15c852435ca30981f5a7b2a04b00731bdf
Bartlomiej Michalski <bartlomiej.michalski@intel.com>
2021-07-28 20:50:37 +0200

    Add Runner struct with implementation of exec function

--- src/utils/ioutils.rs
+++ src/utils/ioutils.rs
@@ -10,3 +10,32 @@ pub fn fill_buffer(reader: &mut impl Read, buffer: &mut Vec<u8>) -> Result<usize
 
     return Ok(bytes_read);
 }
+
+pub struct Runner {}
+
+pub trait CommandExecuter {
+    fn exec(&self, cmd_name: &str, args: &Vec<&str>, input: &mut Vec<u8>) -> Result<Vec<u8>>;
+}
+
+impl CommandExecuter for Runner {
+    fn exec(&self, cmd_name: &str, args: &Vec<&str>, input: &mut Vec<u8>) -> Result<Vec<u8>>{
+        
+        let mut child = Command::new(cmd_name)
+            .args(args)
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn()?;
+
+        let mut stdin = child.stdin.take().expect("Failed to open stdin");
+        
+        let mut input_copy = input.to_vec();
+
+        std::thread::spawn(move || {            
+                stdin.write_all(input_copy.as_mut_slice()).expect("Failed to write to stdin");
+        });
+
+        let output = child.wait_with_output().expect("Failed to read stdout");
+
+        Ok(output.stdout)
+    }
+}
