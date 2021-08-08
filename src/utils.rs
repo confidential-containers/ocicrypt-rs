b0980599e5909dd2701d0f91b5ba8fa6e69942b7
rudy jantz <porter@gatech.edu>
2021-08-07 23:37:24 -0400

    basic success test case passes; 3 major fixes: env bug fix, switch to sha1 (256 not supported), and proper mechanism args
    Signed-off-by: rudy jantz <porter@gatech.edu>

--- src/utils.rs
+++ src/utils.rs
@@ -8,6 +8,7 @@ use rsa::{PublicKey, RsaPublicKey, RsaPrivateKey, pkcs8::FromPublicKey,
 use crate::pkcs11_uri_wrapped::Pkcs11UriWrapped;
 use rand::rngs::OsRng;
 use sha2::Sha256;
+use sha1::Sha1;
 
 
 extern crate serde_yaml;
@@ -167,7 +168,7 @@ fn set_env_vars(env: &HashMap<String, String>) -> Option<std::env::Vars> {
         return None;
     }
     let oldenv = std::env::vars();
-    for (key, value) in std::env::vars() {
+    for (key, value) in env {
         std::env::set_var(key, value);
         // FIXME ? is there no way to check set-var's return code?
     }
@@ -244,20 +245,13 @@ fn pkcs11_uri_login(p11uriw: &Pkcs11UriWrapped,
                     private_key_operation: bool,
 ) -> Result<(pkcs11::Ctx, pkcs11::types::CK_SESSION_HANDLE)> {
     let p11uri = &p11uriw.p11uri;
-    let pin_module_slotid
+    let (pin, module, slotid)
       = pkcs11_uri_get_login_parameters(p11uriw, private_key_operation)?;
-    let pin = pin_module_slotid.0;
-    let module = pin_module_slotid.1;
-    let slotid = pin_module_slotid.2;
 
     let mut p11ctx = pkcs11::Ctx::new(module)?;
-    let session = 64;
 
     let _ = p11ctx.initialize(None);
 
-    // FIXME: This should not be a >= 0 check. slotid will always be unsigned
-    // with rust pkcs11 uri. This should instead be an error check on
-    // pkcs11-uri-get-login-parameters.
     match slotid {
         Some(sid) => {
             if sid > 0xffffffff {
@@ -356,6 +350,7 @@ fn find_object(p11ctx: &pkcs11::Ctx,
     Err(anyhow!("Could not find any object with {}", msg))
 }
 
+
 fn construct_oaep_params(
 ) -> Result<(pkcs11::types::CK_RSA_PKCS_OAEP_PARAMS,
              pkcs11::types::CK_RSA_PKCS_OAEP_PARAMS)
@@ -364,7 +359,6 @@ fn construct_oaep_params(
     // static-global. Is there a better way than doing this dynamically?
 
     // oaep_label defines the label we use for OAEP encryption; cannot be changed
-    // TODO
     let oaep_label: *mut pkcs11::types::CK_VOID = std::ptr::null_mut();
     let label_len: pkcs11::types::CK_ULONG = 0;
 
@@ -372,7 +366,7 @@ fn construct_oaep_params(
     // needed by SoftHSM
     let oaep_sha1_params: pkcs11::types::CK_RSA_PKCS_OAEP_PARAMS
       = pkcs11::types::CK_RSA_PKCS_OAEP_PARAMS {
-        hashAlg: pkcs11::types::CKM_SHA1_RSA_PKCS,
+        hashAlg: pkcs11::types::CKM_SHA_1,
         mgf: pkcs11::types::CKG_MGF1_SHA1,
         source: pkcs11::types::CKZ_DATA_SPECIFIED,
         pSourceData: oaep_label,
@@ -383,7 +377,7 @@ fn construct_oaep_params(
     // algorithm
     let oaep_sha256_params: pkcs11::types::CK_RSA_PKCS_OAEP_PARAMS
       = pkcs11::types::CK_RSA_PKCS_OAEP_PARAMS {
-        hashAlg: pkcs11::types::CKM_SHA256_RSA_PKCS,
+        hashAlg: pkcs11::types::CKM_SHA256,
         mgf: pkcs11::types::CKG_MGF1_SHA256,
         source: pkcs11::types::CKZ_DATA_SPECIFIED,
         pSourceData: oaep_label,
@@ -435,16 +429,17 @@ fn rsa_public_encrypt_oaep(pubkey: &RsaPublicKey,
                                       present or is invalid unicode")),
     };
 
-    // FIXME skipping for now and hardcoding to 256 below
+    let padding: PaddingScheme;
+    let hashalg: &str;
+
     match oaephash.to_lowercase().as_str() {
         "sha1" => {
-            //let hashfunc = sha1.New();
-            //let hashalg = "sha1";
-            return Err(anyhow!("sha1 not supported"));
+            hashalg = "sha1";
+            padding = PaddingScheme::new_oaep::<sha1::Sha1>();
         },
         "sha256" => {
-            //let hashfunc = sha256.New();
-            //let hashalg = "sha246";
+            hashalg = "sha256";
+            padding = PaddingScheme::new_oaep::<sha2::Sha256>();
         },
         _ => {
             return Err(anyhow!("Unsupported OAEP hash '{}'", oaephash));
@@ -452,11 +447,8 @@ fn rsa_public_encrypt_oaep(pubkey: &RsaPublicKey,
     }
 
     let mut rng = OsRng;
-    //let padding = PaddingScheme::new_oaep::<sha1::Sha1>();
-    let padding = PaddingScheme::new_oaep::<sha2::Sha256>();
     let ciphertext = pubkey.encrypt(&mut rng, padding, plaintext)?;
-    // FIXME : hardcoding to sha256 for now.
-    Ok((ciphertext, "sha256".to_string()))
+    Ok((ciphertext, hashalg.to_string()))
 
 
 }
@@ -506,7 +498,7 @@ fn public_encrypt_oaep(pub_key: &Pkcs11KeyFileObject,
         // type CK_RSA_PKCS_OAEP_PARAMS. Do we want the size of the struct
         // here?  (Alternatively, do we want the size of pSourceData, i.e.
         // ulSourceDataLen, or something else?)
-        ulParameterLen: 0,
+        ulParameterLen: std::mem::size_of_val(&oaep) as u64,
     };
     let _ = p11ctx.encrypt_init(session, &mech, p11_pub_key)?;
     let ciphertext = p11ctx.encrypt(session, plaintext)?;
@@ -572,6 +564,7 @@ pub fn encrypt_multiple(pub_keys: &Vec<KeyType>,
     Ok(serde_json::to_vec(&pkcs11_blob)?)
 }
 
+
 // Use a pkcs11 URI describing a private key to OAEP decrypt a ciphertext
 fn private_decrypt_oaep(priv_key: &Pkcs11KeyFileObject,
                         ciphertext: &Vec<u8>,
@@ -599,18 +592,18 @@ fn private_decrypt_oaep(priv_key: &Pkcs11KeyFileObject,
 
     let mut oaep = oaep(hashalg)?;
 
-    // FIXME: boilerplate similar to encrypt_init
-    let oaep_p: *mut pkcs11::types::CK_RSA_PKCS_OAEP_PARAMS = &mut oaep;
+    // FIXME: boilerplate similar to public_encrypt_oaep
+
+    let oaep_p = &mut oaep
+                 as pkcs11::types::CK_RSA_PKCS_OAEP_PARAMS_PTR
+                 as pkcs11::types::CK_VOID_PTR;
+
     let mech = pkcs11::types::CK_MECHANISM {
         mechanism: pkcs11::types::CKM_RSA_PKCS_OAEP,
-        // FIXME this can't be right
-        pParameter: oaep_p as *mut pkcs11::types::CK_VOID,
-        // FIXME: This should be something like "oaep.len()", but oaep is of
-        // type CK_RSA_PKCS_OAEP_PARAMS. Do we want the size of the struct
-        // here?  (Alternatively, do we want the size of pSourceData, i.e.
-        // ulSourceDataLen, or something else?)
-        ulParameterLen: 0,
+        pParameter: oaep_p,
+        ulParameterLen: std::mem::size_of_val(&oaep) as u64,
     };
+
     let _ = p11ctx.decrypt_init(session, &mech, p11_priv_key)?;
 
     let plaintext = p11ctx.decrypt(session, &ciphertext)?;
@@ -675,7 +668,7 @@ pub fn decrypt_pkcs11(priv_keys: &Vec<Pkcs11KeyFileObject>,
                 Ok(x) => {
                     return Ok(x);
                 },
-                Err(_) => {
+                Err(e) => {
                     // TODO
                     //if uri, err2 := privKeyObj.Uri.Format(); err2 == nil {
                     //    errs += fmt.Sprintf("%s : %s\n", uri, err)
